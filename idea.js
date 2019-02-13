2d3b8f6fbebb5cdf42dc3b643d40525a5b2329fc
Erik Konner <erikkonner@gmail.com>
2019-02-12 17:44:42 -0700

    styled inputs, built class and constructor. still need the input values to pass through

--- idea.js
+++ idea.js
@@ -1,5 +1,12 @@
 class Idea {
-  constructor() {
-    this.saveToStorage = 
+  constructor(title, body, quality, cardId) {
+    this.title = title;
+    this.body = body;
+    this.quality = quality;
+    this.cardId = cardId;
+  }
+  saveToStorage() {
+    var stringifyIdea = JSON.stringify(ideas);
+    localStorage.setItem('post', Idea);
   }
 }
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // query select both inputs and sav btn
 var inputIdeas = document.querySelectorAll('.input-selector');
 var saveBtn = document.querySelector('#save-btn');
- console.log(inputIdeas);
+var ideas = [];
 
 
 
@@ -9,32 +9,36 @@ var saveBtn = document.querySelector('#save-btn');
 // event listener for save button that generates a new card and populates with inputs
 // event listener will also save the data in local storage
 
-saveBtn.addEventListener('click', saveToStore)
+saveBtn.addEventListener('click', createIdea)
 
 // create function to save inputs in local storage
-function saveToStore() {
+function createIdea() {
   var title = document.querySelector('.title-style');
   var body = document.querySelector('.body-style');
-  // var newPost = {};
+  var newIdea = new Idea(title, body, "Swill", Date.now())
+  console.log(title);
+  ideas.push(newIdea)
+
+
   // for (var i = 0; i < inputIdeas.length; i++) {
   //   var currentPost = inputIdeas[i];
   //   var postId = currentPost.id;
   //   newPost[postId] = currentPost.value;
   // }
 
-  var stringIdea = JSON.stringify(newPost);
-  console.log(newPost);
-
-  localStorage.setItem('post', stringIdea);
-
-  var post = localStorage.getItem('post');
-
-  post = JSON.parse(post);
+  // var stringIdea = JSON.stringify(newPost);
+  // console.log(newPost);
+  //
+  // localStorage.setItem('post', stringIdea);
+  //
+  // var post = localStorage.getItem('post');
+  //
+  // post = JSON.parse(post);
 
 
 
   // post.push(newPost);
-  console.log(post);
+  // console.log(post);
 }
 
 // create function to JSON the local storage inputs
